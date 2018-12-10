d30bda7f20a472f643a0b76588444759000aea32
Sadeed <sadeed.hakimi@yahoo.com>
2018-12-10 11:57:37 -0800

    cpp

---
+++ Container With Most Water/getWaterArea.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+        return 0;
+    }
+};
+
+int main(){
+
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+    Solution sol;
+    int area = sol.maxArea(height);
+    cout << "Max area is: " << area << endl;
+
+
+    return 0;
+}
+
+
