774693b044cbbe1332b47b573355dbc107a4c50c
Sadeed <sadeed.hakimi@yahoo.com>
2019-01-04 22:15:16 -0800

    final cpp file added

--- Letter Combinations of a Phone Number/phone.cpp
+++ Letter Combinations of a Phone Number/phone.cpp
@@ -1,1 +1,44 @@
-p
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.size() == 0) return {};
+        vector<string> mainVec{""};
+        vector<string> charsVec{" ","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        for (int i = 0; i < digits.length(); i++) {
+            int currentDigit = digits[i] - '0';
+            if (currentDigit < 0 || currentDigit > 9) break;
+
+            string &str = charsVec[currentDigit];
+            vector<string> newVec;
+
+            for (int x = 0; x < mainVec.size(); x++) {
+                for (int z = 0; z < str.size(); z++) {
+                    newVec.push_back(mainVec[x] + str[z]);
+                }
+            }
+            mainVec.swap(newVec);
+        }
+        return mainVec;
+    }
+};
+
+int main(){
+    string s = "23";
+    Solution sol;
+    vector<string> v = sol.letterCombinations(s);
+    for (auto & str : v) {
+        cout << str << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
