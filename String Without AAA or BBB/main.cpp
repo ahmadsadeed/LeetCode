a0f151abad33e4329fb50bd3b5319aee9d20ee42
Sadeed <sadeed.hakimi@yahoo.com>
2019-01-28 22:59:42 -0800

    updated cpp

--- String Without AAA or BBB/main.cpp
+++ String Without AAA or BBB/main.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
     string strWithout3a3b(int A, int B) {
-
-        char a = 'a';
-        char b = 'b';
         string s = "";
-        int len = A + B;
+        int len = A+B;
         for (int i = 0; i < len; i++) {
-
             if (s.size() >= 2) {
-                if (B > A) {
-
-                }
-                if ((s[i-1] == a && s[i-2] == a) && B != 0) {
-                    s += (b);
-                    B--;
-                } else if ((s[i-1] == b && s[i-2] == b) && A != 0){
-                    s += (a);
-                    A--;
+                if ((s[i-1] == 'a' && s[i-2] == 'a') && B != 0) {
+                    s += ('b'); B--;
+                } else if ((s[i-1] == 'b' && s[i-2] == 'b') && A != 0){
+                    s += ('a'); A--;
                 } else {
                     if (A > B) {
-                        s += "a";
-                        A--;
+                        s += "a"; A--;
                     } else {
-                        s += "b";
-                        B--;
+                        s += "b"; B--;
                     }
                 }
             } else {
                 if (A > B && A != 0) {
-                    s += "a";
-                    A--;
+                    s += "a"; A--;
                 } else {
                     if (B != 0) {
-                        s += "b";
-                        B--;
+                        s += "b"; B--;
                     }
-
                 }
             }
         }
